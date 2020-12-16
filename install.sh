fc0088385258f1e2b6941e72863a2983287ccdb5
themmj <themmj@users.noreply.github.com>
2020-12-16 02:22:46 +0100

    Add parsing framework - Add classification of line elements - Add skeleton for state machine - Generalize file access - Improve logging

--- install.sh
+++ install.sh
@@ -7,11 +7,10 @@ PLATFORM=
 PROFILE=
 DEBUG=
 #   parsing utilities
-TMPDIR=$(mktemp -d -t setup-helper-XXXXXXXXXX)
+TMPDIR=$(mktemp -d -t sh-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX)
 platformfile="platforms"
 profilefile="profiles"
-dirfile="dirs"
-appfile="apps"
+setupfile="setup"
 #   platform information
 platformname=
 defaultinstallcmd=
@@ -20,16 +19,30 @@ randomdefault=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 32)
 chosendirs=
 chosenapps=
 
+# log
+padding="    "
+logdepth=0
 log() {
-    echo "[$1] $2"
+    tmp=$2
+    for (( depth=0; depth < $logdepth; depth++ )); do
+        tmp="$padding$tmp"
+    done
+    echo "[$1] $tmp"
+}
+
+incrdepth() {
+    logdepth=$((logdepth + 1))
+}
+decrdepth() {
+    logdepth=$((logdepth - 1))
 }
 
 info() {
-    log "INFO" "$@"
+    log "INFO " "$@"
 }
 
 warn() {
-    log "WARN" "$@"
+    log "WARN " "$@"
 }
 
 error() {
@@ -56,6 +69,10 @@ print_help() {
     echo "      enable debug logs"
 }
 
+# substitution values
+bssub=${TMPDIR: -32}
+bsdqsub=randomdefault
+
 # file io utilities
 scriptfilehandle=
 set_script_file_handle() {
@@ -63,11 +80,14 @@ set_script_file_handle() {
 }
 
 write_to_script_file() {
+    # replace subs with actual values
+    tmp="${1//$bssub/\\}"
+    tmp="${tmp//$bsdqsub/\"}"
     # if override flag is provided
     if [ $# -eq 2 ]; then
-        echo "$1" > "$scriptfilehandle"
+        echo "$tmp" > "$scriptfilehandle"
     else
-        echo "$1" >> "$scriptfilehandle"
+        echo "$tmp" >> "$scriptfilehandle"
     fi
 }
 
@@ -78,6 +98,7 @@ set_config_file_handle() {
 
 configexists=
 check_config_file() {
+    incrdepth
     if [ ! -f "$configfilehandle" ]; then
         # if fatal flag is set
         if [ $# -eq 1 ]; then
@@ -90,13 +111,103 @@ check_config_file() {
     else
         configexists=1
     fi
+    decrdepth
 }
 
+linenumber=
+process_config_file() {
+    linenumber=1
+    OIFS=$IFS
+    while IFS= read -r line
+    do
+        parse_line "$line"
+        process_line_data
+    done < $configfilehandle
+    IFS=$OIFS
+}
+
+# state transition logic
+qcurr=
+qfin=
+
+linenumber=
 istag=
 tagname=
+isvalue=
 rowvalues=
+iskeyword=
+currkeyword=
 parse_line() {
-    # TODO
+    debug "parse line $linenumber"
+    incrdepth
+    OIFS=$IFS
+    istag=0
+    isvalue=0
+    case $1 in
+        *\[*\])
+            istag=1
+            tmp="${1#*\[}"
+            tagname="${tmp%\]*}"
+            debug "tagname $tagname"
+            ;;
+        *\"*)
+            isvalue=1
+            rowvalues=()
+            debug "row values $1"
+            tmp="${1//\\\"/$bsdqsub}"
+            tmp="${tmp//\\/$bssub}"
+            IFS='\"'
+            read -ra split <<< "$tmp"
+            skip=1
+            for i in "${split[@]}"; do
+                if [ $skip -eq 1 ]; then
+                    skip=0
+                else
+                    skip=1
+                    rowvalues+=("$i")
+                    debug "extracted row value $i"
+                fi
+            done
+            ;;
+        *:)
+            iskeyword=1
+            tmp="${1// }"
+            currkeyword="${tmp%\:*}"
+            debug "found keyword $currkeyword"
+            ;;
+        "")
+            ;;
+        *)
+            error "unrecognized pattern on line $linenumber: $1"
+            exit -1
+            ;;
+    esac
+    linenumber=$((linenumber + 1))
+    IFS=$OIFS
+    decrdepth
+}
+
+process_line_data() {
+    debug "process line $linenumber"
+    incrdepth
+    if [ $istag -eq 1 ]; then
+        case $qcurr in
+            *)
+                debug "default case for tag $tagname"
+                ;;
+        esac
+    elif [ $iskeyword -eq 1 ]; then
+        debug "keyword for state transition $currkeyword"
+    elif [ $isvalue -eq 1 ]; then
+        for value in "${rowvalues[@]}"; do
+            case $qcurr in
+                *)
+                    debug "default case for value $value"
+                    ;;
+            esac
+        done
+    fi
+    decrdepth
 }
 
 # path logic
@@ -104,6 +215,7 @@ isinhome=
 isinroot=
 pathcache=
 check_path() {
+    incrdepth
     isinhome=0
     isinroot=0
     case "$1" in
@@ -129,44 +241,38 @@ check_path() {
     fi
     debug "isinhome: $isinhome isinroot: $isinroot for $1"
     debug "$1 was normalized to $pathcache"
+    decrdepth
 }
 
 parse_platforms() {
+    incrdepth
     info "parsing platforms"
     set_config_file_handle $platformfile
     check_config_file "x"
-
-    # TODO
+    process_config_file
+    decrdepth
 }
 
 parse_profiles() {
+    incrdepth
     info "parsing profiles"
     set_config_file_handle $profilefile
     check_config_file
 
     if [ $configexists -eq 1 ]; then
-        # TODO
+        process_config_file
     fi
+    decrdepth
 }
 
-parse_dirs() {
+parse_setup() {
+    incrdepth
     info "parsing directories"
-    set_config_file_handle $dirfile
+    set_config_file_handle $setupfile
     check_config_file "x"
-
-    set_script_file_handle $dirfile
-
-    # TODO
-}
-
-parse_apps() {
-    info "parsing apps"
-    set_config_file_handle $appfile
-    check_config_file "x"
-
-    set_script_file_handle $appfile
-
-    # TODO
+    set_script_file_handle $setupfile
+    process_config_file
+    decrdepth
 }
 
 # init arguments
@@ -214,27 +320,27 @@ info "parse config start"
 
 parse_platforms
 parse_profiles
-parse_dirs
-parse_apps
+parse_setup
 
 info "parse config end"
 
 run_script() {
+    incrdepth
     set_script_file_handle $1
     if [ -f "$scriptfilehandle" ]; then
         /bin/bash $scriptfilehandle
     else
         warn "tried to run config which did not exist $scriptfilehandle"
     fi
+    decrdepth
 }
 
 # running parsed config files
-info "running configs start"
+info "running config start"
 
-run_script "$dirfile"
-run_script "$appfile"
+run_script "$setupfile"
 
-info "running configs end"
+info "running config end"
 
 rm -rf "$TMPDIR"
 
